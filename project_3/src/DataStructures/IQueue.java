10b87364b53b89614862430b230d2c33dd57ded6
Gorea Tudor <tudortudor2002@gmail.com>
2021-10-30 15:36:18 +0300

    implemented IQueue in Queue class

--- project_3/src/DataStructures/IQueue.java
+++ project_3/src/DataStructures/IQueue.java
@@ -7,5 +7,6 @@ public interface IQueue {
     void enqueue(String item); // adds a new item to the queue
     String dequeue(); // extracts an item from the queue
     String peek(); // take a look at the node that has to be extracted
+    int size(); // returns the size of the queue (number of elements)
     boolean isEmpty(); // verifies if the queue is empty
 }
--- project_3/src/DataStructures/Queue.java
+++ project_3/src/DataStructures/Queue.java
@@ -1,28 +1,44 @@
 package DataStructures;
 
-public class Queue implements IQueue{
+
+import java.util.LinkedList;
+
+public class Queue implements IQueue {
+
+    private LinkedList <String> queue;
 
     public Queue () {
-        ;
+        queue = new LinkedList<>();
     }
 
     @Override
     public void enqueue(String item) {
-
+        synchronized (queue) {
+            queue.add(item);
+        }
     }
 
     @Override
     public String dequeue() {
-        return null;
+        synchronized (queue) {
+            return queue.remove();
+        }
     }
 
     @Override
     public String peek() {
-        return null;
+        synchronized (queue) {
+            return queue.peek();
+        }
+    }
+
+    @Override
+    public int size() {
+        return queue.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return queue.isEmpty();
     }
 }
