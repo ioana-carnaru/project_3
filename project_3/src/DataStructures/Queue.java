18d274b2fa8a52461e38af428a7997bfa8a8532f
Gorea Tudor <tudortudor2002@gmail.com>
2021-10-30 15:12:24 +0300

    added IQueue

---
+++ project_3/src/DataStructures/Queue.java
@@ -0,0 +1,28 @@
+package DataStructures;
+
+public class Queue implements IQueue{
+
+    public Queue () {
+        ;
+    }
+
+    @Override
+    public void enqueue(String item) {
+
+    }
+
+    @Override
+    public String dequeue() {
+        return null;
+    }
+
+    @Override
+    public String peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
