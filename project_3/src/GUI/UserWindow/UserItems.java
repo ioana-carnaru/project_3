856632ad710c078f4e23b0cb9d141d3fd4fdf77e
Gorea Tudor <tudortudor2002@gmail.com>
2021-11-01 18:38:52 +0200

    userframe #6 - can now see the inventory of a person

--- project_3/src/GUI/UserWindow/UserItems.java
+++ project_3/src/GUI/UserWindow/UserItems.java
@@ -2,30 +2,51 @@ package GUI.UserWindow;
 
 import GUI.AbstractClasses.Frames;
 
+import javax.swing.*;
 import java.awt.*;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class UserItems extends Frames {
 
     private static UserItems frame;
     private ResultSet resultSet;
+    private ArrayList <JLabel> items = new ArrayList<>();
+    private JPanel panel;
 
 
 
     private UserItems(String title, ResultSet resultSet) {
-        super(title);
+        super(title, 300, 300);
         this.resultSet = resultSet;
-        this.setLayout(new FlowLayout());
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
         try {
+//            while (resultSet.next()) {
+//                System.out.println(resultSet.getString("username") + " " + resultSet.getInt("iditem") + " " + resultSet.getInt("quantity"));
+//            }
+            items.add(new JLabel("iditem - quantity"));
+            int counter = 0;
+            JLabel label;
+
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("username") + " " + resultSet.getInt("iditem") + " " + resultSet.getInt("quantity"));
+                label = new JLabel((++counter) + ":  " + resultSet.getInt("iditem") + "  -  " + resultSet.getInt("quantity"));
+                label.setVerticalAlignment(JLabel.TOP);
+                label.setHorizontalAlignment(JLabel.CENTER);
+                items.add(label);
             }
+
+            for (JLabel labelIterator: items) { // add labels to the panel
+                panel.add(labelIterator);
+            }
+
         } catch (Exception exception) {
             exception.printStackTrace();
         }
 
 
+        this.add(panel);
         this.setVisible(true);
     }
 
