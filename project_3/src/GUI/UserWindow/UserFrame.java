a5e92c261c60c3502cd4392a6fe93b4d9f3298da
VasileBarabas <vasilebarabas@yahoo.com>
2021-11-01 17:36:51 +0200

    Incercare de resultSet pentru inventory

--- project_3/src/GUI/UserWindow/UserFrame.java
+++ project_3/src/GUI/UserWindow/UserFrame.java
@@ -14,9 +14,9 @@ public class UserFrame extends Frames {
     private static UserFrame frame;
     private String username = null;
 
-    private ResultSet resultSet;
+    private ResultSet resultSet=null;
 
-    AddItemButton addItemButton;
+    //AddItemButton addItemButton;
 
 
     private UserFrame (String username) {
@@ -25,23 +25,17 @@ public class UserFrame extends Frames {
         this.setSize(300, 300);
         this.setLayout(new FlowLayout());
 
-        synchronized (queue) {
-            queue.enqueue(IQueue.showInventory);
+   /* try {
+        Thread.sleep(1000);
+        while (resultSet!=null) {
+            System.out.println(resultSet.getString("username") + " " + resultSet.getInt("iditem") + " " + resultSet.getInt("quantity"));
         }
-
-        try {
-            while (resultSet == null || resultSet.next() == false)
-                Thread.sleep(10);
-
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(0) + " " + resultSet.getString(1) + " " + resultSet.getString(2));
-            }
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        this.add(addItemButton);
+    }
+    catch (SQLException | InterruptedException ex)
+    {
+        ex.printStackTrace();
+    }*/
+      //this.add(addItemButton);
         this.setVisible(true);
     }
 
@@ -60,4 +54,16 @@ public class UserFrame extends Frames {
     public void setResultSet(ResultSet resultSet) {
         this.resultSet = resultSet;
     }
+//    public void ShowRes()
+//    {
+//        try {
+//            while (resultSet.next()) {
+//                System.out.println(resultSet.getString("username") + " " + resultSet.getInt("iditem") + " " + resultSet.getInt("quantity"));
+//            }
+//        }
+//        catch (SQLException ex)
+//        {
+//            ex.printStackTrace();
+//        }
+//    }
 }
